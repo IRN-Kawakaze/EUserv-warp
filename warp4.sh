0d70f6028d7d8363c1d996df718443f49aa0a5bf
IRN-Kawakaze <41798687+IRN-Kawakaze@users.noreply.github.com>
2021-07-13 05:04:13 +0800

    删掉部分依赖

--- warp4.sh
+++ warp4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -e "\033[1;32m 脚本仅支持 Ubuntu20 与 Debain10 系统;\n 主要用于OpenVZ、LXC架构的IPV6 only VPS;\n WARP仅接管IPV4网络! \033[0m"
-apt update && apt install curl sudo lsb-release iptables -y
+apt update && apt install curl sudo iptables -y
 apt install openresolv dnsutils wireguard-tools --no-install-recommends -y
 wget -6 https://cdn.jsdelivr.net/gh/IRN-Kawakaze/EUserv-warp/wgcf
 wget -6 https://cdn.jsdelivr.net/gh/IRN-Kawakaze/EUserv-warp/wireguard-go
